f65df1f0919ad20ea40764efae4f205b69b97423
wolvs5 <krish.shrestha@metropolia.fi>
2024-11-30 18:10:49 +0200

    Update ex1.js

--- assignment2/ex1/ex1.js
+++ assignment2/ex1/ex1.js
@@ -1,14 +1,11 @@
-document.getElementById('submitButton').addEventListener('click', printNumbers);
+const numbers = []
 
-function printNumbers() {
+for (let i= 0; i<5; i++) {
+    let userInput = prompt('Please enter five numbers.');
+    numbers.push(Number(userInput));
+}
 
-    const num1 = document.getElementById('number1').value;
-    const num2 = document.getElementById('number2').value;
-    const num3 = document.getElementById('number3').value;
-    const num4 = document.getElementById('number4').value;
-    const num5 = document.getElementById('number5').value;
-
-
-    console.log("Numbers entered:");
-    console.log(num1, num2, num3, num4, num5);
+console.log('Numbers in reverse order:');
+for (let i = numbers.length - 1; i>=0; i--) {
+    console.log(numbers[i]);
 }
